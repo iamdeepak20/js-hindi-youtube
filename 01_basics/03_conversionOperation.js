669781b69ab64acf7543c38384c5508f5a5ea339
Deepak Kumar Mandal <159442791+iamdeepak20@users.noreply.github.com>
2024-09-14 05:47:31 +0000

    discuss conversing

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "Deepak"
+
+//console.log(typeof score)
+//console.log(typeof(score))
+
+let valueInNumber = Number(score)
+//console.log(typeof(valueInNumber))
+//console.log(valueInNumber)
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLogedIn = "deepak"
+let booleanIsLogedIN = Boolean(isLogedIn)
+
+//console.log(booleanIsLogedIN)
+
+
+// 1 => true; 0 => false
+// "" => false
+// "deepak" => true
+
+let number = 55
+let numberInString = String(number)
+//console.log(typeof numberInString)
+//console.log(numberInString)
+
