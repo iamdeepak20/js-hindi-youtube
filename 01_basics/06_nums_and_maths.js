b9aad949401b51a508d07fc14d0a96d3b49d1093
Deepak Kumar Mandal <159442791+iamdeepak20@users.noreply.github.com>
2024-09-15 06:13:00 +0000

    nums and maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,44 @@
+const score = 500
+//console.log(score);
+
+const blance = new Number(200)
+//console.log(blance);
+
+// console.log(blance.toString().length);
+// console.log(blance.toFixed(1));
+
+const otherNumber = 345.34556
+//console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-In'));
+
+
+//*************** maths *****************
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.max(2,5,8,9));
+// console.log(Math.min(8,5,6,));
+
+// console.log(Math.random());
+// console.log(Math.random()*10 + 1 );
+// console.log(Math.floor(Math.random()*10) +1) ;
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
+
+
+
+
+
+
+
+
