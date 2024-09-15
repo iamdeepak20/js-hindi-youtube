639e5240177f7012fc92c4a6c6c8b5d18199dbd4
Deepak Kumar Mandal <159442791+iamdeepak20@users.noreply.github.com>
2024-09-15 05:13:47 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,42 @@
+const name = "Deepak"
+const repoCount = 20
+
+//console.log(name + repoCount + "Value");
+
+console.log(`Hello my name is ${name} and my repocount is ${repoCount}`);
+
+const gameName = new String("deepak-dk-com")
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf("p"));
+
+const newString = gameName.substring(0,4)
+// console.log(newString)
+
+const anotherString = gameName.slice(0,4)
+// console.log(anotherString);
+
+const newStringOne = "   Deepak   "
+// console.log(newStringOne)
+// console.log(newStringOne.trim());
+
+const url = "https://deepak.com/deepak%20mandal"
+
+console.log(url.replace("%20","_"));
+
+console.log(url.includes("sundar"));
+
+console.log(gameName.split("-"));
+
+
+
+
+
+
+
