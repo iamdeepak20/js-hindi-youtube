bab00afbb3a0d26bb8e0f767979a772a4849a863
Deepak Kumar Mandal <159442791+iamdeepak20@users.noreply.github.com>
2024-09-19 11:57:40 +0000

    for in

---
+++ 05_iteration/04_four.js
@@ -0,0 +1,28 @@
+const myObj = {
+    js:"javascript",
+    cpp:"c++",
+    rb:"ruby",
+    swift:"swift by apple"
+}
+for (const key in myObj) {
+ // console.log(`${key} shorcuts is for : ${myObj[key]}`);
+  
+}
+
+const myArray = ["Ind","USA","Nep","Afg"]
+for (const key in myArray) {
+    //console.log(key,myArray[key]);
+    
+}
+
+
+// const map = new Map()
+// map.set('Ind','India')
+// map.set('Nep','Nepal')
+// map.set('Afg','Afganishtan')
+
+// for (const element in object) {
+//     console.log(key);
+    
+// }
+//don't work
