a8cbfb99260b27810ec9f6209c115f1ef1301e5e
Deepak Kumar Mandal <159442791+iamdeepak20@users.noreply.github.com>
2024-09-19 11:53:32 +0000

    while loop and do-while loop

---
+++ 05_iteration/02_two.js
@@ -0,0 +1,20 @@
+let i = 1;
+while(i<=10){
+    //console.log(`value of i is ${i}`);
+    i = i+1;
+}
+
+let myArray = ["Rohit","Kohli","Dhoni"]
+let index = 0;
+while(index<myArray.length){
+    //console.log(`value is ${myArray[index]}`);
+    index = index + 1
+}
+
+
+let score = 15
+do {
+    console.log(`Score is ${score}`);
+    score++
+    
+} while (score<10);
