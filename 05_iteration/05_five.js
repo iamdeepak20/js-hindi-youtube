f2495fa68909e40f5bde91da786e4901d3b3b5b1
Deepak Kumar Mandal <159442791+iamdeepak20@users.noreply.github.com>
2024-09-19 11:59:11 +0000

    discuss about forEach

---
+++ 05_iteration/05_five.js
@@ -0,0 +1,44 @@
+const coding = ["c","c++","java","python","js"]
+
+// coding.forEach(function (item){
+//     console.log(item);
+    
+// })
+
+
+// coding.forEach( (items) => {
+//     console.log(items);
+    
+// })
+
+// function printMe(item){
+//     console.log(item);  
+// }
+// coding.forEach(printMe)
+
+
+// coding.forEach((item,index,arr)=>{
+//     console.log(item,index,arr);
+    
+// })
+
+
+const mynewArray = [
+    {
+        name:"Deepak",
+        age:21
+    },
+    {
+        name:"Sumit",
+        age:22
+    },
+    {
+        name:"virat",
+        age:36
+    }
+]
+
+mynewArray.forEach((items) =>{
+    console.log(items.name);
+    
+})
